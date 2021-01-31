40ffe2dec0a523514b6ffca219e8871d10bab8bd
Maria Mamina <maria.mamina3@gmail.com>
2021-01-31 09:58:23 +0200

    UI development

--- Color Mixer/ViewController.swift
+++ Color Mixer/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redColorNumber: UILabel!
+    @IBOutlet var greenColorNumber: UILabel!
+    @IBOutlet var blueColorNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
     }
 
-
+    @IBAction func redSlider() {
+    }
+    
+    @IBAction func greenSlider() {
+    }
+    
+    @IBAction func blueSlider() {
+    }
 }
 
