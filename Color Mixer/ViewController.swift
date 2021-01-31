feb17f7965979e46f67780b0c046dcb7cfa3ae28
Maria Mamina <maria.mamina3@gmail.com>
2021-01-31 13:17:14 +0200

    Realisation logic of work

--- Color Mixer/ViewController.swift
+++ Color Mixer/ViewController.swift
@@ -10,22 +10,43 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
+    
     @IBOutlet var redColorNumber: UILabel!
     @IBOutlet var greenColorNumber: UILabel!
     @IBOutlet var blueColorNumber: UILabel!
     
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.layer.cornerRadius = 10
+        colorView.layer.cornerRadius = 15
     }
 
-    @IBAction func redSlider() {
+    @IBAction func redColorAction() {
+        redColorNumber.text = String(format: "%.2f", redSlider.value)
+        updateColor()
     }
     
-    @IBAction func greenSlider() {
+    @IBAction func greenSliderAction(){
+        greenColorNumber.text = String(format: "%.2f", greenSlider.value)
+        updateColor()
     }
     
-    @IBAction func blueSlider() {
+    @IBAction func blueColorAction() {
+        blueColorNumber.text = String(format: "%.2f", blueSlider.value)
+        updateColor()
     }
+    
+    func updateColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = color
+    }
+    
 }
 
